a99d211ac888d24da8136d6b289d5cf3dbcbb213
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-01-16 10:44:28 -0500

    Update Setup

---
+++ tailwind.config.js
@@ -0,0 +1,80 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      tangerine: ['Tangerine', 'cursive'],
+      ballet: ['Ballet', 'cursive'],
+    },
+    extend: {
+      backgroundImage: {
+        'background': "url('/img/background.AVIF')",
+      },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+      animation: {
+        'waving-hand': 'wave 2s linear infinite',
+      },
+      colors: {
+        primary: '#E0DED1',
+        secondary: '#787065',
+        title: '#4E4943',
+        logo: '#3DB6D1',
+        greenDark: '#287384',
+      },
+      container: {
+        center: true,
+        width: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1440px',
+          '2xl': '2560px',
+        },
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+        spacing: {
+          sm: '8px',
+          md: '16px',
+          lg: '24px',
+          xl: '48px',
+        },
+        borderRadius: {
+          'none': '0',
+          'sm': '0.125rem',
+          DEFAULT: '0.25rem',
+          DEFAULT: '4px',
+          'md': '0.375rem',
+          'lg': '0.5rem',
+          'full': '9999px',
+          'large': '12px',
+        },
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
